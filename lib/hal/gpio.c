ad2e4e37c3268225ca0781c21071cf9ccb0e3785
Ayelen <ayelenvegacaro@gmail.com>
2024-05-13 14:27:45 -0300

    cambios

--- lib/hal/gpio.c
+++ lib/hal/gpio.c
@@ -2,3 +2,15 @@
 #include "hal.h"
 
 /* Aquí tu implementación de controlador para puerto GPIO */
+
+static void habilitaRelojPuertoC(void)
+{
+    RCC->APB2ENR = RCC->APB2ENR | RCC_APB2ENR_IOPCEN;
+}
+
+typedef struct pin{
+    GPIO_TypeDef*puerto;
+    int pin;
+    void (*habilitaReloj)(void);
+}pin;
+
--- lib/hal/timer_systick.c
+++ lib/hal/timer_systick.c
@@ -1,5 +1,19 @@
 #include <stm32f1xx.h>
 #include "hal.h"
 
-/* Aquí tu implementación de controlador para el timer SysTick */
+
+// Función para inicializar el temporizador SysTick
+void HAL_SysTick_Init(uint32_t ticks) {
+    // Configurar el temporizador SysTick para que genere una interrupción cada 'ticks' milisegundos
+    SysTick->LOAD = (ticks * (SystemCoreClock / 1000)) - 1; // Configurar el valor de recarga del contador
+    SysTick->VAL = 0; // Reiniciar el contador
+    SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_TICKINT_Msk | SysTick_CTRL_ENABLE_Msk; // Habilitar el temporizador
+}
+
+// Función de interrupción del temporizador SysTick
+void SysTick_Handler(void) {
+    // Implementa aquí la lógica de la interrupción del temporizador SysTick
+    // Por ejemplo, cambiar el estado del LED, etc.
+}
+
 
